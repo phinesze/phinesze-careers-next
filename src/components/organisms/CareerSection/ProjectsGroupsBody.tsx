bf3f684e3d8e3bb688a9883b62f7e85ab7cc1a98
inoueshinichi <phinesze@gmail.com>
2025-10-19 17:31:35 +0900

    ProjectsGroupsBodyに会社名を追加

--- src/components/organisms/CareerSection/ProjectsGroupsBody.tsx
+++ src/components/organisms/CareerSection/ProjectsGroupsBody.tsx
@@ -1,10 +1,26 @@
 "use client";
 
 import { ProjectGroup } from "@/types/ProjectGroup";
+import { useCareerTableSectionState } from "@/composables/useCareerTableSectionState";
 
 type Props = {
   groups: ProjectGroup[];
 }
+
 export default function CareerSectionProjectsGroupsBody({ groups } : Props) {
-  return <></>
+  const { isSecrets } = useCareerTableSectionState()
+  return <>
+    {
+      groups.map((group) => {
+        return <section key={group.companyAlias}>
+          {/* {group.companyAlias} */}
+          {/* 会社名 */}
+          <div className={"bg-blue-100 border-y p-2 font-bold break-after-avoid"}>
+            { isSecrets ? group.company ?? group.companyAlias : group.companyAlias }
+          </div>
+        </section>
+
+      })
+    }
+  </>
 }
