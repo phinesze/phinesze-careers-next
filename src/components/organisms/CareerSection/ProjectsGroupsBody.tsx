b690caad24f1a1959651026b8221f889e3a5c996
inoueshinichi <phinesze@gmail.com>
2025-11-03 20:17:49 +0900

    update

--- src/components/organisms/CareerSection/ProjectsGroupsBody.tsx
+++ src/components/organisms/CareerSection/ProjectsGroupsBody.tsx
@@ -7,33 +7,45 @@ import MarkdownDocument from "@/components/atoms/MarkdownDocument";
 
 type Props = {
   groups: ProjectGroup[];
-}
+};
 
 export default function CareerSectionProjectsGroupsBody({ groups }: Props) {
   const { isSecrets } = useCareerTableSectionState();
   let projectIndex = 0;
-  return <>
-    {
-      groups.map((group) => {
-        return <section key={group.companyAlias}>
-          {/* {group.companyAlias} */}
-          {/* 会社名 */}
-          <div className={"bg-blue-100 border-y p-2 font-bold break-after-avoid"}>
-            {isSecrets ? group.company ?? group.companyAlias : group.companyAlias}
-            ProjectsGroupsBody
-          </div>
-          {/* 会社のプロジェクト */}
-          {
-            group.projects.map((project) => {
-                projectIndex++;
-                return <section key={project.id}
-                                className={"career-row break-inside-avoid border-t-[1px] border-b-[1px]"}>
+  return (
+    <>
+      {groups.map((group) => {
+        return (
+          <section key={group.companyAlias}>
+            {/* {group.companyAlias} */}
+            {/* 会社名 */}
+            <div
+              className={"bg-blue-100 border-y p-2 font-bold break-after-avoid"}
+            >
+              {isSecrets
+                ? (group.company ?? group.companyAlias)
+                : group.companyAlias}
+              ProjectsGroupsBody
+            </div>
+            {/* 会社のプロジェクト */}
+            {group.projects.map((project) => {
+              projectIndex++;
+              return (
+                <section
+                  key={project.id}
+                  className={
+                    "career-row break-inside-avoid border-t-[1px] border-b-[1px]"
+                  }
+                >
                   {/* 文章行部分 */}
                   <div className={"align-top flex p-0"}>
                     {/* 番号・期間 */}
                     <div className={"w-[20mm]"}>
                       <div
-                        className={"flex items-center border-r-2 h-full bg-lime-300 text-center"}>
+                        className={
+                          "flex items-center border-r-2 h-full bg-lime-300 text-center"
+                        }
+                      >
                         <div className={"text-center"}>
                           #{projectIndex}
                           <IntervalDateLabel value={project.times} />
@@ -55,13 +67,12 @@ export default function CareerSectionProjectsGroupsBody({ groups }: Props) {
                       </div>
                     </div>
                   </div>
-                </section>;
-              },
-            )
-          }
-        </section>;
-
-      })
-    }
-  </>;
+                </section>
+              );
+            })}
+          </section>
+        );
+      })}
+    </>
+  );
 }
